40c8444f36510cbee715b49058f1efeb06dcbb5b
Damien R <dam.ray49@gmail.com>
2021-02-22 21:16:25 +0000

    Add README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Train reservation
+This is a repository to illustrate the article available here https://dnomyar.github.io/posts/2021-02-21-hexagonal-architecture.html.
--- src/main/scala/train/reservation/domain/SeatReservationService.scala
+++ src/main/scala/train/reservation/domain/SeatReservationService.scala
@@ -3,15 +3,15 @@ package train.reservation.domain
 import train.reservation.domain.BookedSeatRepository
 import train.reservation.domain.model.Seat
 import train.reservation.domain.model.Customer
+import train.reservation.domain.model.SeatBooked
 
 class SeatReservationService(bookedSeatRepository: BookedSeatRepository) {
     def book(seat: Seat, customer: Customer): Unit = {
-        // val seatBooked = bookedSeatRepository.find(seat).isDefined
-        // if(! seatBooked){
-        //     bookedSeatRepository.save(
-        //         SeatBooked(seat, customer)
-        //     )
-        // }
-
+        val seatBooked = bookedSeatRepository.find(seat).isDefined
+        if(! seatBooked){
+            bookedSeatRepository.save(
+                SeatBooked(seat, customer)
+            )
+        }
     }    
 }
